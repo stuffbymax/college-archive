7f397466db5c8ed74063aa8c9426d75331b1112a
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2025-01-30 11:04:52 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 # image source:
 # map.jpg: https://visibleearth.nasa.gov/view.php?id=57752 Credit: NASA
 import turtle
+import urllib.request
+import json
+
 screen = turtle.Screen()
 screen.setup(720, 360)
+screen.bgpic('map.gif')
 screen.setworldcoordinates(-180, -90, 180, 90)
-screen.bgpic('map.jpg')
+
+url = 'http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(url)
+astros = json.loads(response.read())
+
+
+num_people = turtle.Turtle()
+num_people = turtle.Turtle()
+num_people.penup()
+num_people.hideturtle()
+num_people.color('yellow')
+num_people.goto(-175,-25)
+num_people.write('people in space: ' + str(astros['number']))
